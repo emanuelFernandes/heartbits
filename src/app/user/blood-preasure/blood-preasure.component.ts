ee7d39a55238e6a3d1517d304c0955df000df4d1
emanuelFernandes <emanuel_queiroga1@hotmail.com>
2017-11-19 04:01:42 +0000

    bloodPreasure colors

--- src/app/user/blood-preasure/blood-preasure.component.ts
+++ src/app/user/blood-preasure/blood-preasure.component.ts
@@ -16,21 +16,66 @@ export class BloodPreasureComponent implements OnInit {
 
   public tableData2: TableData;
 
+   MAXSIST = 140;
+   MAXDIST = 90;
+   EMERGENCYSIST = 180;
+   EMERGENCYDIST = 110;
+
   ngOnInit() {
 
   	this.tableData2 = {
             headerRow: [ 'Data', 'Manhã',  'Tarde', 'Noite', 'FC' ],
             dataRows: [
                 ['15/11/2017', '130/70','135/72', '130/73', '70' ],
-                ['16/11/2017', '135/74','137/71', '140/73', '74'],
+                ['16/11/2017', '135/74','145/71', '140/73', '74'],
                 ['17/11/2017', '133/70','130/76', '129/74', '72' ],
-                ['18/11/2017', '140/72','133/71', '130/73', '68' ],
-                ['19/11/2017', '130/70','135/72', '136/73', '70'],
+                ['18/11/2017', '183/95','133/71', '130/95', '68' ],
+                ['19/11/2017', '130/70','145/72', '136/111', '70'],
             ]
         };
 
   }
 
+
+  getStyle(cell){
+  	const value = cell.split('/')[0];
+
+  	//console.log(value);
+
+  	if(value > this.EMERGENCYSIST){
+  		return {color:'red'};
+  	}
+
+  	if(value > this.MAXSIST)
+  	{
+  		return {'font-weight':'bold'};
+  	}	
+  }
+
+  sist(cell){
+  	return cell.split('/')[0];
+  }
+
+
+
+  getStyleDist(cell){
+  	const value = cell.split('/')[1];
+
+  	//console.log(value);
+
+  	if(value > this.EMERGENCYDIST){
+  		return {color:'red'};
+  	}
+
+  	if(value > this.MAXDIST)
+  	{
+  		return {'font-weight':'bold'};
+  	}	
+  }
+
+  sistDist(cell){
+  	return cell.split('/')[1];
+  }
    
 
 }
